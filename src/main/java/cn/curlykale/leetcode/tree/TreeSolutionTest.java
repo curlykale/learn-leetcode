122a5ef1e037333e924d1debe27614dbe4d8f599
maxp <curlykale@foxmail.com>
2020-05-06 23:43:40 +0800

    节点数求二叉搜索树数量

--- src/main/java/cn/curlykale/leetcode/tree/TreeSolutionTest.java
+++ src/main/java/cn/curlykale/leetcode/tree/TreeSolutionTest.java
@@ -116,6 +116,54 @@ public class TreeSolutionTest {
         logger.info(JSON.toJSONString(treeArray(treeNode)));
     }
 
+
+    @Test
+    public void testNumTrees() {
+        int numTrees = numTrees(4);
+        logger.info(String.valueOf(numTrees));
+    }
+
+    /**
+     *
+     * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+     *
+     * 示例:
+     *
+     * 输入: 3
+     * 输出: 5
+     * 解释:
+     * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+     *
+     *    1         3     3      2      1
+     *     \       /     /      / \      \
+     *      3     2     1      1   3      2
+     *     /     /       \                 \
+     *    2     1         2                 3
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
+     *
+     *  卡特兰数 公式
+     * G(n) = G(0)*G(n-1)+G(1)*G(n-2)+...+G(n-1)*G(0)G(n)=G(0)∗G(n−1)+G(1)∗(n−2)+...+G(n−1)∗G(0)
+     *
+     * 作者：guanpengchn
+     * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees/solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
+     *
+     * @param n 节点数
+     * @return 组成的二叉搜索树数量
+     */
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
     /**
      * 给定一个二叉树，原地将它展开为链表。
      * <p>
